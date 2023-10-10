a23a6638c66bc0b24281e990524ff45fe26b420a
julia <julia.navarro@clever-cloud.com>
2023-10-10 17:18:40 +0200

    create dashboard view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 /*  Routes tell the Router which view to display when a user clicks a link
 or pastes a URL into the browser address bar. */
 const routes: Routes = [
   // Path needs to be a string that matches the URL in the browser address bar
+  { path: 'dashboard', component: DashboardComponent },
   { path: 'heroes', component: HeroesComponent }
+  
 ];
 
 // Initialize the router and starts it listening for browser location changes
