beaa2073a49a8f076a52d55e6f81135590c3c062
julia <julia.navarro@clever-cloud.com>
2023-10-10 17:04:12 +0200

    add AppRoutingModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
 
-const routes: Routes = [];
+/*  Routes tell the Router which view to display when a user clicks a link
+or pastes a URL into the browser address bar. */
+const routes: Routes = [
+  // Path needs to be a string that matches the URL in the browser address bar
+  { path: 'heroes', component: HeroesComponent }
+];
 
+// Initialize the router and starts it listening for browser location changes
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  // Import RouterModel and configure it with the routes
+  imports: [RouterModule.forRoot(routes)], // Configure the router at the application's root level
+  exports: [RouterModule] // Export RouterModule to be available throughout the application
 })
+
 export class AppRoutingModule { }
