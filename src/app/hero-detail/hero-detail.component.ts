7ee331dabfffc4f1fbeef06faa83f51610ee7294
julia <julia.navarro@clever-cloud.com>
2023-10-10 17:51:48 +0200

    extract the id route parameter

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { HeroService } from '../hero.service';
 import { Hero } from '../hero';
 
 @Component({
@@ -7,7 +11,25 @@ import { Hero } from '../hero';
   styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent {
+  constructor(
+    private route: ActivatedRoute, // holds information about the route
+    private heroService: HeroService, // gets hero data from the remote server
+    private location: Location //  Angular service for interacting with the browser
+  ) {}
+
+  ngOnInit(): void {
+    this.getHero();
+  }
+  
+  getHero(): void {
+    // Static image of the route information
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+  }
+
   @Input() hero?: Hero;
+
 
 }
 
