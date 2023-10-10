0971a2f9a97997d64016cefa30b763f8e71f4d6e
julia <julia.navarro@clever-cloud.com>
2023-10-10 15:52:14 +0200

    add message service and methods

--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -3,13 +3,17 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root',
 })
+
+/* The service exposes its cache of messages and two methods:*/
 export class MessageService {
   messages: string[] = [];
 
+  /* 1. Add a message to the cache*/
   add(message: string) {
     this.messages.push(message);
   }
-
+  
+  /* 2. Clear the cache*/
   clear() {
     this.messages = [];
   }
