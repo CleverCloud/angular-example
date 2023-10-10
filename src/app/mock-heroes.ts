f05a75c2cc21121c6d7f7ecb9330893b2de3d470
julia <julia.navarro@clever-cloud.com>
2023-10-10 14:49:13 +0200

    add CSS classes for selected heroes

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'juuduu' },
+  { id: 13, name: 'wonshtrum' },
+  { id: 14, name: 'Akanoa' },
+  { id: 15, name: 'ArendSyl' },
+  { id: 16, name: 'GwinizDu' },
+  { id: 17, name: 'P4nda' },
+  { id: 18, name: 'Kannar' },
+  { id: 19, name: 'cnivolle' },
+  { id: 20, name: 'blackyoup' }
+];
