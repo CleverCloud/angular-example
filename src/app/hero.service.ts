a8e11fbf330362f02bbe4b69fbb13d82c7581ad0
julia <julia.navarro@clever-cloud.com>
2023-10-10 15:36:41 +0200

    replace synchronous method by async

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { Observable, of } from 'rxjs';
 
+/* Service here is provided at the root level.
+Angular creates a single, shared instance of HeroService and injects 
+into any class that asks for it.
+Registering the provider in the @Injectable metadata also allows Angular
+to optimize an application by removing the service if it isn't used. */
 @Injectable({
   providedIn: 'root'
 })
+
 export class HeroService {
 
-  constructor() { }
+  
+
+
+getHeroes(): Observable<Hero[]> {
+  const heroes = of(HEROES);
+  return heroes;
+  }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,12 +8,35 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-  
-  heroes = HEROES;
+
   selectedHero?: Hero;
+  
+  heroes: Hero[] = [];
+  
+
+  constructor(private heroService: HeroService) {}
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 
   onSelect(hero: Hero): void {
   this.selectedHero = hero;
+
   }
 
-};
+  getHeroes(): void {
+    /* Synchronous signature.
+    This approach won't work in a real application that uses asynchronous calls.
+    It works now because our service synchronously returns mock heroes.*/
+    /*this.heroes = this.heroService.getHeroes();*/
+
+    /*Asynchronous signature: 
+    Use this to avoid a UI freeze while it waits for the server's reponse!*/
+    
+      this.heroService.getHeroes()
+          .subscribe(heroes => this.heroes = heroes);
+    }
+
+  
+}
